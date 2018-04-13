5f53cd4d1d043417c76fe21192c110d1246b68a1
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-04-13 14:37:32 +0530

    Made Changes

--- src/com/example/hash/HashMapDemo.java
+++ src/com/example/hash/HashMapDemo.java
@@ -7,14 +7,15 @@ public class HashMapDemo {
 	public static  void main(String[] args) {
 		// TODO Auto-generated method stub
 		HashMap<Object, Integer> hashMap = new HashMap<>(10);
-		hashMap.put(11, 5000);
+		/*hashMap.put(11, 5000);
 		hashMap.put(11, 9966);
 		hashMap.put(528, 8888);
 		hashMap.put(7, 1542);
-		//hashMap.put("asdasdasdasd", 55224444);
+		*///hashMap.put("asdasdasdasd", 55224444);
 		String s = "sriniG";
 		hashMap.display();
 		System.out.println(s.hashCode());
+		hashMap.get(s);
 		hashMap.get(s);
 		/*hashMap.display();
 		hashMap.remove(4);
