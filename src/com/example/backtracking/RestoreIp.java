25426467e2a016c148cb2b6743a58e197b31ada1
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-04-24 16:44:48 +0530

    Added

---
+++ src/com/example/backtracking/RestoreIp.java
@@ -0,0 +1,33 @@
+package com.example.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestoreIp {
+
+
+    List<String> formIP(String s){
+
+        List<String> res = new ArrayList<>();
+        doRestore(res, "", s, 0);
+        return res;
+    }
+
+    private void doRestore(List<String> res, String path, String s, int k) {
+
+        if(s.isEmpty() || k == 4){
+            if (s.isEmpty() && k == 4){
+                res.add(path.substring(1));
+            }
+            return;
+        }
+        for(int i=1;i <= (s.charAt(0) == '0' ? 1 : 3) && i <= s.length(); i++){
+            String part = s.substring(0, i);
+            if(Integer.valueOf(part) <= 255){
+                doRestore(res, path + "." + part, s.substring(i), k + 1);
+            }
+        }
+    }
+
+
+}
---
+++ src/com/example/backtracking/RestoreIpDemo.java
@@ -0,0 +1,13 @@
+package com.example.backtracking;
+
+public class RestoreIpDemo {
+
+    public static void main(String[] args) {
+        String words = "19816012";
+
+        RestoreIp ip = new RestoreIp();
+
+        System.out.println(ip.formIP(words));
+    }
+
+}
