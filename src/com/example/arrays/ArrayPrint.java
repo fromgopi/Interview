b634c4abe3837982bb6b4de0888b86f47421a546
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-04-30 09:20:15 +0530

    Improved

--- src/com/example/arrays/ArrayPrint.java
+++ src/com/example/arrays/ArrayPrint.java
@@ -5,8 +5,10 @@ public class ArrayPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a[] = {1,2,3,4,5,8,6,9,4,2,40,100};
-		for(int i : a){
-			System.out.println(a[i]);
+		for(int firstItem : a){
+			for(int secondItem : a){
+				System.out.println(firstItem + ", " + secondItem);
+			}
 		}
 	}
 
