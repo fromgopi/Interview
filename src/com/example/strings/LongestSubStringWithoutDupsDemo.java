3f45b862a9ee3a71286f88dc5fdf4f24d9e6376f
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-05-20 21:53:52 +0530

    Added more

---
+++ src/com/example/strings/LongestSubStringWithoutDupsDemo.java
@@ -0,0 +1,14 @@
+package com.example.strings;
+
+public class LongestSubStringWithoutDupsDemo {
+
+    public static void main(String[] args) {
+
+        String str = "aaabccdefgaaabc";
+
+        LongestSubStringWithoutDups ls = new LongestSubStringWithoutDups();
+
+        ls.findSubString(str);
+
+    }
+}
