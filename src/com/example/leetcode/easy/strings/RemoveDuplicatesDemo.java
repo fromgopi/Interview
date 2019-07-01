84dda5aa1fd50eb1ffcae354ce541c533de87c90
MUKTEVI GOPI KRISHNA <muktevigk@handbookinc.com>
2019-07-01 11:26:00 +0530

    Added more design Questions

---
+++ src/com/example/leetcode/easy/strings/RemoveDuplicatesDemo.java
@@ -0,0 +1,11 @@
+package com.example.leetcode.easy.strings;
+
+public class RemoveDuplicatesDemo {
+
+    public static void main(String[] args) {
+
+        RemoveDuplicates rd = new RemoveDuplicates();
+        System.out.println(rd.remove("aabbcca"));
+
+    }
+}
