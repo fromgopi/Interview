1893fabb4e98f4089112fd4f74a52a6f8eac33b6
Gopi Krishna M <gopt.muktevi@gmail.com>
2020-08-28 00:03:01 +0530

    Adding some hard problems

---
+++ interview-2.0/src/com/example/strings/TwoStringsDemo.java
@@ -0,0 +1,13 @@
+package com.example.strings;
+
+public class TwoStringsDemo {
+    public static void main(String[] args) {
+
+        String str1 = "wouldyoulikefries";
+        String str2 = "world";
+
+        TwoStrings twoStrings = new TwoStrings();
+
+        System.out.println(twoStrings.solution(str1, str2));
+    }
+}
