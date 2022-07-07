1495d062b19e4bd61e17c4ef7cd6fb45702716f5
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-07-07 20:44:44 +0530

    Update Surveillance.java

--- src/patterns/behavioral/observer/Surveillance.java
+++ src/patterns/behavioral/observer/Surveillance.java
@@ -3,7 +3,7 @@ package patterns.behavioral.observer;
 public class Surveillance extends CheckList implements AlarmListener{
     @Override
     public void alarm() {
-        System.out.println("Surveillance - by the numbers:");
+        System.out.println("Surveillance - by the numbers:"); 
         byTheNumbers();
     }
 
